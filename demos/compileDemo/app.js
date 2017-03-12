35940e2adfdbfac13e29fb2c6f71eb6d711d230e
Shayan Ghazi <shayansta@gmail.com>
2017-03-11 22:34:01 -0500

    demo dependencies
    restructured and added package.json

--- demos/compileDemo/app.js
+++ demos/compileDemo/app.js
@@ -1,23 +1,33 @@
 var express = require('express');
 var path = require('path');
 var compiler = require('compilex');
+//var mongoose = require('mongoose');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
- 
-var options = {stats : true};
-compiler.init(options);
+//mongoose.connect('mongodb://root:abc@localhost/admin');
+//var db = mongoose.connection;
 
-var envData = { OS : "linux" };
+
 
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/compilerView.html'));
+  res.sendFile(path.join(__dirname + '/views/compilerView.html'));
 })
 
 app.get('/compile', function (req, res) {
-  compiler.compilePython( envData , req.query.code , function(data){ res.send(data); });
+  var options = {stats : true};
+  compiler.init(options);
+  var envData = { OS : "linux" };
+
+  compiler.compilePython( envData , req.query.code , 
+        function(data){ res.send(data); });
 })
+
+
+
+
+
 
 app.listen(8888, function () {
   console.log('Example app listening on port 8888!')
