644147bb4a40e1ef751a9d7ca9e49aba1f78c4ae
Abdulqader Saafan <abdulqader.saafan@mail.utoronto.ca>
2017-03-12 15:28:47 -0400

    Added setup script

---
+++ setup.bash
@@ -0,0 +1,5 @@
+echo '############### Running First time Install Script ###############';
+sudo npm install -g angular-cli;
+cd grace;
+sudo npm install;
+echo '############### Good to go ###############';
