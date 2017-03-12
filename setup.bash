c1201d0509eec8cb34f54608aae1b4ff9712d7ad
Abdulqader Saafan <abdulqader.saafan@mail.utoronto.ca>
2017-03-12 15:51:46 -0400

    Updated bash script

--- setup.bash
+++ setup.bash
@@ -1,6 +1,6 @@
 echo '############### Running First time Install Script ###############';
 sudo apt-get install nodejs -y;
-sudo npm install angular-cli;
+sudo npm install angular-cli -g;
 cd grace;
 sudo npm install;
 echo '############### Good to go ###############';
