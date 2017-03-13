e912e1f1c17f3339141f5c540fb65b956f06082c
Abdulqader Saafan <abdulqader.saafan@mail.utoronto.ca>
2017-03-13 19:14:31 -0400

    More skeleton code

--- grace/src/routes/api.js
+++ grace/src/routes/api.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 //var mongojs = require('mongojs');
 
+/******************* Login/Register	*******************/
+
 router.get('/api', function(req,res){
 	res.send("USER API");
 });
@@ -10,5 +12,20 @@ router.get('/test', function(req, res){
 	res.send(req.query.id);
 	// Test this by going to http://localhost:3000/test?id="abode"
 });
+
+/******************* Compiler *******************/
+
+router.get('/compile', function(req, res){
+	// Compile code here
+	// code = req.query.code
+});
+
+
+
+
+
+
+
+
 
 module.exports = router;
