e6789ccedbe04633ef255a19d1ad5f95e3b43b94
Abdulqader Saafan <abdulqader.saafan@mail.utoronto.ca>
2017-03-13 18:48:03 -0400

    Initial code for compile service

--- grace/src/app/services/compile.service.ts
+++ grace/src/app/services/compile.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Http, URLSearchParams } from '@angular/http';
 
 @Injectable()
 export class CompileService {
 
-  constructor() { }
+	constructor(private http: Http) { }
+
+	compileCode(code: string) {
+		let params: URLSearchParams = new URLSearchParams();
+		params.set('user', 'guest');
+		params.set('code', code);
+		return this.http.get('../api/api.js', {
+			search: params
+		}).subscribe(
+			(response) => this.onGetCompileResult(response.json()),
+			(error) => this.onGetCompilerError(error.json())
+		);
+	}
+
+	onGetCompileResult(result: string){
+		// Set text area to hold results
+	}
+
+	onGetCompilerError(error: string){
+		// Set text area to show error maybe alert?
+	}
 
 }
