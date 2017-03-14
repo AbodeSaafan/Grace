9d231e324b8170246aaba2a9245b45b96303fa0c
Julius <barboza4>
2017-03-14 00:39:01 -0400

    added get function

--- grace/src/routes/api.js
+++ grace/src/routes/api.js
@@ -1,12 +1,31 @@
 var express = require('express');
 var router = express.Router();
-//var mongojs = require('mongojs');
+var mongojs = require('mongojs');
 var compiler = require('compilex');
 
 /******************* Login/Register	*******************/
+var db = mongojs('users',['users']);
+  
+router.get('/signin', function(req,res){
+	db.users.find(function(err,users){
+		if(err){
+			res.send(err);
+		}
+		else{
+			res.json(users);
+		}
+	});
+});
 
-router.get('/api', function(req,res){
-	res.send("USER API");
+router.get('/signin/:id', function(req,res){
+	db.users.findOne({_id: req.parms.id},function(err,user){
+		if(err){
+			res.send(err);
+		}
+		else{
+			res.json(user);
+		}
+	});
 });
 
 router.get('/test', function(req, res){
@@ -19,7 +38,7 @@ router.get('/test', function(req, res){
 router.get('/compile', function(req, res){
 	// Compile code here
 	// code = req.query.code
-	var options = {stats : true};
+	var options = {stats : true}
 	compiler.init(options);
 	var envData = { OS : "linux" };
 
