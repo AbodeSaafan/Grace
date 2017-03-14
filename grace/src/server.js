1b942984b8a01d8b7804a04f4550afa02cff7e83
Abdulqader Saafan <abdulqader.saafan@mail.utoronto.ca>
2017-03-13 21:58:27 -0400

    Allowed cross site requests

--- grace/src/server.js
+++ grace/src/server.js
@@ -13,6 +13,12 @@ app.set('view engine','ejs');
 app.engine('html', require('ejs').renderFile);
 */	
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParse.json());
 app.use(bodyParse.urlencoded({extended: false}));
 
