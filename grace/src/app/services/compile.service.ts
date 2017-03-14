029d379fe6e619f1412aea44fd1a2823dada12fe
Abdulqader Saafan <abdulqader.saafan@mail.utoronto.ca>
2017-03-14 00:29:15 -0400

    Updated compiler service

--- grace/src/app/services/compile.service.ts
+++ grace/src/app/services/compile.service.ts
@@ -5,13 +5,15 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class CompileService {
 
+	apiURL:string = "http://localhost:3000/compile";
+
 	constructor(private http: Http) { }
 
 	compileCode(code: string) {
 		let params: URLSearchParams = new URLSearchParams();
 		params.set('user', 'guest');
 		params.set('code', code);
-		return this.http.get('http://localhost:3000/compile', {
+		return this.http.get(this.apiURL, {
 			search: params
 		}).map(res => res.json());
 	}
