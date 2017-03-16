f4762405eb7ef560417c39400370ee26b85d88b5
nfrd <vincent.nafrada@mail.utoronto.ca>
2017-03-16 01:33:57 -0400

    added settings component

---
+++ grace/src/app/components/settings/settings.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { FileStorageService } from '../../services/file-storage.service';
+import { HeaderConfig } from '../header/header.component';
+import { MaterialModule} from '@angular/material';
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.css']
+})
+export class SettingsComponent implements OnInit {
+
+	constructor(){}
+
+  	ngOnInit() {}
+ 	
+}
