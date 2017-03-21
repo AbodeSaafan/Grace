47ad65700debc5a37d20f826c96cd4c14ac5a99e
nfrd <vincent.nafrada@mail.utoronto.ca>
2017-03-20 21:45:40 -0400

    settings

--- grace/src/app/components/settings/settings.component.ts
+++ grace/src/app/components/settings/settings.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer } from '@angular/core';
 import { FileStorageService } from '../../services/file-storage.service';
 import { HeaderConfig } from '../header/header.component';
 import { MaterialModule} from '@angular/material';
+
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css']
 })
-export class SettingsComponent implements OnInit {
 
-	constructor(){}
+export class SettingsComponent implements OnInit {
+  @ViewChild('sidenav') el:ElementRef;
+
+	constructor(private rd: Renderer){}
 
   	ngOnInit() {}
 
@@ -24,4 +27,15 @@ export class SettingsComponent implements OnInit {
   	changeTheme(){
   		
   	}
+    ngAfterViewInit(){
+      // alert("works");
+      //this.rd.invokeElementMethod(this.el.nativeElement,'open');
+      // this.el.open();
+      //sidenav.open();
+    }
+
+    openSide(sidenav: any){
+      sidenav.open();
+    }
+    
 }
