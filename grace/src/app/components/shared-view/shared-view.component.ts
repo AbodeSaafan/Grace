5002188adcfce5cb8e2d4827948343d58d2a2064
Abdulqader Saafan <abdulqader.saafan@mail.utoronto.ca>
2017-03-21 21:13:31 -0400

    Adding some shared-view code

--- grace/src/app/components/shared-view/shared-view.component.ts
+++ grace/src/app/components/shared-view/shared-view.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FileStorageService } from '../../services/file-storage.service';
+import { Router } from '@angular/router';
+import { HeaderConfig, HeaderComponent } from '../header/header.component';
+import { MdSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-shared-view',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SharedViewComponent implements OnInit {
 
-  constructor() { }
+	sharedHeader: HeaderConfig;
+	@Input() fileIDFromURL: string;
+	fileCode: string;
+
+  constructor(private router: Router, private fileStorage: FileStorageService, 
+  			  public snackBar: MdSnackBar) {
+  	this.sharedHeader = {
+      leftButtonContent: "none",
+      rightButtonContent: "Sign Up",
+      leftButtonSrc: "",
+      rightButtonSrc: ""
+    }
+
+    
+  }
 
   ngOnInit() {
+  	// Set fileCode in localstorage and make sure name is in there too
+    this.fileCode = this.fileIDFromURL;
+  }
+
+  signUpClicked(){
+  	this.router.navigateByUrl('/');
+  }
+
+  logoClicked(){
+  	this.router.navigateByUrl('/');
+  }
+
+  saveClicked(){
+  	this.snackBar.open("You must create your own file to save", 'Okay' ,{duration: 5000});
   }
 
 }
