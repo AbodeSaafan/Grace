fb4478ca9d6cc4181c1fcfd517efe9e0edab9694
Shayan Ghazi <shayansta@gmail.com>
2017-03-21 20:47:04 -0400

    new landing buttons
    - toggle views for register and login forms

--- grace/src/app/components/landing/landing.component.ts
+++ grace/src/app/components/landing/landing.component.ts
@@ -10,14 +10,16 @@ import {AuthorizeService} from '../../services/authorize.service';
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css'],
   animations: [routerTransition()],
-  host: {'[@routerTransition]': ''}
+  //host: {'[@routerTransition]': '/'}
 })
-export class LandingComponent implements OnInit {
+export class LandingComponent {
 
   constructor(private router: Router, private authService: AuthorizeService) {
    }
 
   ngOnInit() {
+    $("#loginForm").hide();
+    $("#registerForm").hide();
     // Auto-login
     this.authService.isAuthenticated().subscribe(data => {
       this.router.navigateByUrl('/dash');
@@ -36,8 +38,8 @@ export class LandingComponent implements OnInit {
 	}
 
 	loginClicked() {
-    alert("sdfds");
-    $("#registerForm").slideToggle();
+    $("#registerForm").hide();
+    $("#loginForm").slideToggle();
 		// console.log("login clicked!!");
 		// if (this.shown == 'LOGIN'){
   //     this.shown = 'NONE';
@@ -49,6 +51,8 @@ export class LandingComponent implements OnInit {
 	}
 
 	registerClicked() {
+    $("#loginForm").hide();
+    $("#registerForm").slideToggle();
 		// console.log("register clicked!!");
   //   if (this.shown == 'REGISTER'){
 		//   this.shown = 'NONE';
