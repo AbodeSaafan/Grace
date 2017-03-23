8f5dd2f70c7f65b029cda927bc8b7de7a3e11669
Nafizolith <nafiz.hossain@mail.utoronto.ca>
2017-03-23 06:20:38 -0400

    [#140986563] Added one final touch to seamless navigation- a 404 page

--- grace/src/app/components/not-found/not-found.component.ts
+++ grace/src/app/components/not-found/not-found.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import { MaterialModule } from '@angular/material';
+
 
 @Component({
   selector: 'app-not-found',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { 
+  }
 
   ngOnInit() {
+  	// start off at 10
+  	var setStart = 9;
+  	var countdown = setInterval(() => {
+
+  		var countDisplay = document.getElementById("countdown");
+  		countDisplay.innerHTML = setStart.toString();
+  		
+  		setStart --; 
+  		
+  		if (setStart < 0){
+  			this.router.navigateByUrl("/");
+  			clearInterval(countdown);
+  		}
+
+  	}, 1000);
+
+  }
+
+  onClick(){
+  	this.router.navigateByUrl("/");
+  }
+
+  countDown(){
   }
 
 }
